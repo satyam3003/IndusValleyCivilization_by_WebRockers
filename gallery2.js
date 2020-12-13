8e68cfd6d1fa46af0993c4b8644fa66a8123441b
Shreya Kothawale <75836806+shreyakothawale@users.noreply.github.com>
2020-12-13 12:37:53 +0530

    Add files via upload

---
+++ gallery2.js
@@ -0,0 +1,26 @@
+$(' .btn-dark ').on("click", function () {
+    $(".body-swap").removeClass("body-light").addClass("body-dark");
+    $(".nav-swap").removeClass("navbar-light").addClass("navbar-dark");
+    $(".nav-swap").removeClass("bg-light").addClass("bg-dark");
+    $(".top-img-1-swap").removeClass("top-img-1-light").addClass("top-img-1-dark");
+    $(".main-body-lg-div-swap").removeClass("main-body-lg-div-light").addClass("main-body-lg-div-dark");
+    $(".end-section-swap").removeClass("end-section-light").addClass("end-section-dark");
+    $(".iconcontact-swap").removeClass("iconcontact-light").addClass("iconcontact-dark");
+    $(".comment-display-body-swap").removeClass("comment-display-body-light").addClass("comment-display-body-dark");
+    $(".para-rest-swap").removeClass("para-rest-light").addClass("para-rest-dark");
+    $(".para-text-rest-swap").removeClass("para-text-rest-light").addClass("para-text-rest-dark");
+});
+
+
+$(' .btn-light ').on("click", function () {
+    $(".body-swap").removeClass("body-dark").addClass("body-light");
+    $(".nav-swap").removeClass("navbar-dark").addClass("navbar-light");
+    $(".nav-swap").removeClass("bg-dark").addClass("bg-light");
+    $(".top-img-1-swap").removeClass("top-img-1-dark").addClass("top-img-1-light");
+    $(".main-body-lg-div-swap").removeClass("main-body-lg-div-dark").addClass("main-body-lg-div-light");
+    $(".end-section-swap").removeClass("end-section-dark").addClass("end-section-light");
+    $(".iconcontact-swap").removeClass("iconcontact-dark").addClass("iconcontact-light");
+    $(".comment-display-body-swap").removeClass("comment-display-body-dark").addClass("comment-display-body-light");
+    $(".para-rest-swap").removeClass("para-rest-dark").addClass("para-rest-light");
+    $(".para-text-rest-swap").removeClass("para-text-rest-dark").addClass("para-text-rest-light");
+});
